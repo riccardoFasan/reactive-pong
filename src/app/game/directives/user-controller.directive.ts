c85420fb8bda751f91c8556d6d9a23e04480480d
Riccardo Fasan <57989127+riccardoFasan@users.noreply.github.com>
2022-08-20 19:53:50 +0200

    refactor: removed mouse controls

--- src/app/game/directives/user-controller.directive.ts
+++ src/app/game/directives/user-controller.directive.ts
@@ -1,13 +1,10 @@
 import {
   AfterViewInit,
   Directive,
-  ElementRef,
   HostListener,
   OnDestroy,
 } from '@angular/core';
-import { Platform } from '@ionic/angular';
 import { PaddleController } from '../interfaces';
-import { CollisionService, GameControlsService } from '../services';
 import { BaseControllerDirective } from './base-controller.directive';
 
 @Directive({
@@ -17,27 +14,10 @@ export class UserControllerDirective
   extends BaseControllerDirective
   implements AfterViewInit, OnDestroy, PaddleController
 {
-  constructor(
-    collision: CollisionService,
-    ref: ElementRef,
-    controls: GameControlsService,
-    private platform: Platform
-  ) {
-    super(collision, ref, controls);
-  }
-
-  @HostListener('touchmove', ['$event'])
-  @HostListener('window:mousemove', ['$event'])
-  private onMove(e: Event): void {
-    if (e instanceof TouchEvent) {
-      const touch: Touch = e.touches[0];
-      if (touch) this.movePaddle(touch.clientY);
-      return;
-    }
-    if (!this.platform.is('mobile')) {
-      const halfHeight: number = this.paddleHeight / 2;
-      this.movePaddle((e as MouseEvent).clientY - halfHeight);
-    }
+  @HostListener('window:touchmove', ['$event'])
+  private onMove(e: TouchEvent): void {
+    const touch: Touch = e.touches[0];
+    if (touch) this.movePaddle(touch.clientY);
   }
 
   private movePaddle(positionY: number): void {
