b93459d91e74aac00f0be3fb297df599811e758b
Riccardo Fasan <57989127+riccardoFasan@users.noreply.github.com>
2022-08-21 18:48:43 +0200

    feat: hide status bar

--- src/app/components/app/app.component.ts
+++ src/app/components/app/app.component.ts
@@ -1,24 +1,33 @@
 import { AfterViewInit, Component } from '@angular/core';
 import { ScreenOrientation } from '@awesome-cordova-plugins/screen-orientation/ngx';
+import { StatusBar } from '@capacitor/status-bar';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
 })
 export class AppComponent implements AfterViewInit {
-  hasOrientationBeenAdjusted: boolean = false;
+  hasStyleBeenAdjusted: boolean = false;
 
   constructor(private orientation: ScreenOrientation) {}
 
   async ngAfterViewInit(): Promise<void> {
     await this.adjustStyle();
-    this.hasOrientationBeenAdjusted = true;
+    this.hasStyleBeenAdjusted = true;
   }
 
   private async adjustStyle(): Promise<void> {
     try {
       // some devices like PCs have no orientation
-      await this.orientation.lock(this.orientation.ORIENTATIONS.LANDSCAPE);
+      await Promise.all([this.setOrientation(), this.hideStatusBar()]);
     } catch {}
+  }
+
+  private async setOrientation(): Promise<void> {
+    await this.orientation.lock(this.orientation.ORIENTATIONS.LANDSCAPE);
+  }
+
+  private async hideStatusBar(): Promise<void> {
+    await StatusBar.hide();
   }
 }
