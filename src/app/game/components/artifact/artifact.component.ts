b11eb7334de31486264863574eb635bb84d2af43
Riccardo Fasan <57989127+riccardoFasan@users.noreply.github.com>
2022-09-04 11:43:53 +0200

    refactor: removed useless oninit

--- src/app/game/components/artifact/artifact.component.ts
+++ src/app/game/components/artifact/artifact.component.ts
@@ -1,10 +1,4 @@
-import {
-  AfterViewInit,
-  Component,
-  ElementRef,
-  OnDestroy,
-  OnInit,
-} from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnDestroy } from '@angular/core';
 import { isIonicReady } from 'src/utilities';
 import { Action } from '../../enums';
 import { Artifact, Coordinates } from '../../models';
@@ -15,22 +9,17 @@ import { ArtifactsService } from '../../services';
   templateUrl: './artifact.component.html',
   styleUrls: ['./artifact.component.scss'],
 })
-export class ArtifactComponent
-  implements OnInit, AfterViewInit, OnDestroy, Artifact
-{
+export class ArtifactComponent implements AfterViewInit, OnDestroy, Artifact {
   id!: number;
   action!: Action;
   coordinates!: Coordinates;
 
   constructor(private ref: ElementRef, private artifacts: ArtifactsService) {}
 
-  ngOnInit() {
-    this.artifacts.registerArtifact(this.artifact, this.ref);
-  }
-
   async ngAfterViewInit(): Promise<void> {
     await isIonicReady();
     this.setHostPosition();
+    this.artifacts.registerArtifact(this.artifact, this.ref);
   }
 
   ngOnDestroy(): void {
