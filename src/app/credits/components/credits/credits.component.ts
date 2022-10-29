a063c1d3385bfd7cea6fcb428101759c5bdbf15b
Riccardo Fasan <57989127+riccardoFasan@users.noreply.github.com>
2022-10-29 19:35:07 +0200

    feat: in-app browser

--- src/app/credits/components/credits/credits.component.ts
+++ src/app/credits/components/credits/credits.component.ts
@@ -1,4 +1,5 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { Browser } from '@capacitor/browser';
 
 @Component({
   selector: 'app-credits',
@@ -6,4 +7,18 @@ import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
   styleUrls: ['./credits.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class CreditsComponent {}
+export class CreditsComponent {
+  visitPayPal(): void {
+    this.openInAppBrowser(
+      'https://www.paypal.com/donate/?hosted_button_id=2RR27KLB9JBUU'
+    );
+  }
+
+  visitGitHub(): void {
+    this.openInAppBrowser('https://github.com/riccardoFasan/reactive-pong');
+  }
+
+  private async openInAppBrowser(url: string): Promise<void> {
+    await Browser.open({ url });
+  }
+}
